fa86e21942608e44d22bc8df1af8a8ee44c50852
Adecovemacker <arthur.decovemacker@epitech.eu>
2022-10-04 17:17:37 +0200

    kk

--- x-y.py
+++ x-y.py
@@ -1,15 +1,1 @@
-# importing geopy library
-from geopy.geocoders import Nominatim
- 
-# calling the Nominatim tool
-loc = Nominatim(user_agent="GetLoc")
- 
-# entering the location name
-getLoc = loc.geocode("Gosainganj Lucknow")
- 
-# printing address
-print(getLoc.address)
- 
-# printing latitude and longitude
-print("Latitude = ", getLoc.latitude, "\n")
-print("Longitude = ", getLoc.longitude)
+print('Hello, World!')
